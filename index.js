0631ad8f448dab2ffa36e21c921c3fce6ab1808a
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-03-19 18:58:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const cloudinary = require("cloudinary").v2;
+
+async function UploadImageToCloudinary (file, folder = "image-data", height = "100", quality = "100", cloud_name, api_key, api_secret){
+
+  try {
+    // const { file, folder, height, quality, cloud_name, api_key, api_secret } = req.body;
+
+    if (!cloud_name || !api_key || !api_secret || !file) {
+
+      throw new Error("All parameters are not fulfilled");
+
+
+    }
+
+    cloudinary.config({
+      cloud_name,
+      api_key,
+      api_secret,
+
+    });
+
+    const options = { folder, height, quality, resource_type: "auto" };
+
+    const result = await cloudinary.uploader.upload(file.tempFilePath, options);
+
+    console.log("result is ",result);
+
+    return result;
+
+  } catch (error) {
+    console.log("Cloudinary upload failed");
+    console.error(error);
+    throw new Error(error);
+  }
+
+};
+
+
+
