2c01a06aabf40c41fccabba3a466cdeb7ea7ae63
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-03-24 09:01:56 +0530

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const cloudinary = require("cloudinary").v2;
 
-async function UploadImageToCloudinary (file, folder = "image-data", height = "100", quality = "100", cloud_name, api_key, api_secret){
+export default async function UploadImageToCloudinary (file, folder = "image-data", height = "100", quality = "100", cloud_name, api_key, api_secret){
 
   try {
     // const { file, folder, height, quality, cloud_name, api_key, api_secret } = req.body;
@@ -37,7 +37,7 @@ async function UploadImageToCloudinary (file, folder = "image-data", height = "1
 };
 
 
-module.exports = UploadImageToCloudinary;
+// module.exports = UploadImageToCloudinary;
 
 
 
